800db9747ff0826d5cd0a52b349de7b0941c2578
justin <eqplatus@gmail.com>
2025-12-05 21:33:07 -0600

    Initial Commit

---
+++ TournamentApp/TournamentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentAppApp.swift
+//  TournamentApp
+//
+//  Created by user287050 on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TournamentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
